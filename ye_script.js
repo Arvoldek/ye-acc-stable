69de06b216b0ac9851c8d0a5b67eb930a6348577
Filip Nikolas Parak <filip.parak@yeself.com>
2021-03-30 14:47:10 +0200

    new page

---
+++ ye_script.js
@@ -0,0 +1,11 @@
+(function (i, s, o, g, r, a, m) {
+  i['YesElfApi'] = r; i[r] = i[r] || function () {
+    (i[r].q = i[r].q || []).push(arguments)
+  }, i[r].l = 1 * new Date(); a = s.createElement(o),
+    m = s.getElementsByTagName(o)[0]; a.async = 1; a.id = 'yesElf'; a.src = g; m.parentNode.insertBefore(a, m.nextSibling)
+})
+  (window, document, 'script', 'https://yeself-cloud-acc-app.brux.space:443/sdk/2b9c4603-33cc-4870-8f4f-35b7d875fc86/app.js', 'yesElf');
+yesElf('id', '2b9c4603-33cc-4870-8f4f-35b7d875fc86');
+yesElf('popups', true);
+yesElf('segmentation', 'userId', 'testId');
+yesElf('segmentation', 'roles', 'admin');
